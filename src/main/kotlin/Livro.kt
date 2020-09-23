92d2cb6cc15f8cafa5335019e6b6cd90ef3bbecd
Lucas Coelho <lcsilva2099@gmail.com>
2020-09-23 15:53:40 -0300

    Classe Livro

---
+++ src/main/kotlin/Livro.kt
@@ -0,0 +1,8 @@
+package main.kotlin
+
+data class Livro(val preco: Double,
+            val codigo: Int,
+            val qtd: Int,
+            val titulo: String,
+            val autor: String,
+            val lancamento: Int)
